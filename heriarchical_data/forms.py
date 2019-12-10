e37d43016127c97afdb23699c802d999e87a96e7
Zachary Kline <zkline888@gmail.com>
2019-12-10 13:36:38 -0500

    Added some of the extra credit to the project

---
+++ heriarchical_data/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class CreateFile(forms.Form):
+    name = forms.CharField(max_length=50)
--- heriarchical_data/views.py
+++ heriarchical_data/views.py
@@ -1,26 +1,70 @@
-from mptt.admin import DraggableMPTTAdmin
+# from mptt.admin import DraggableMPTTAdmin
 from heriarchical_data.models import File
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from heriarchical_data.forms import LoginForm, CreateFile
+from django.contrib.auth.models import User
+from django.views import View
+from django.views.generic.edit import CreateView
 
 
-class FileView(DraggableMPTTAdmin):
-    mptt_indent_field = "name"
-    list_display = ('tree_actions', 'indented_title')
-    list_display_links = ('indented_title',)
-
-    def get_queryset(self, request):
-        html = 'index.html'
-        item = File.objects.all()
-        ancestors = item.get_ancestors(ascending=False, include_self=False)
-        children = item.get_children()
-        return render(request, html, {
-            'item': item,
-            'ancestors': ancestors,
-            'children': children
-        })
-
-
+@login_required
 def homeview(request):
     html = 'index.html'
     data = File.objects.all()
-    return render(request, html, {'data': data})
+    fileuser = User.objects.all()
+    return render(request, html, {'data': data, 'fileuser': fileuser})
+
+
+def loginview(request):
+    html = 'login.html'
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                username=data['username'],
+                password=data['password']
+            )
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(
+                    request.GET.get('next', reverse('homepage'))
+                )
+    form = LoginForm()
+    return render(request, html, {'form': form})
+
+
+def logoutview(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('homepage'))
+
+
+class ViewProfile(View):
+    def get(self, request, id):
+        html = 'profile.html'
+        fileuser = User.objects.filter(id=id).first()
+        data = File.objects.filter(user=fileuser)
+        return render(request, html, {
+            'data': data, 'fileuser': fileuser})
+
+
+class FileCreateView(CreateView):
+    html = 'createfile.html'
+
+    def get(self, request):
+        form = CreateFile()
+        return render(request, self.html, {'form': form})
+
+    def post(self, request):
+        form = CreateFile(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            File.objects.create(
+                name=data['name'],
+                user=request.user
+            )
+            return HttpResponseRedirect(reverse('homepage'))
+        form = CreateFile()
+        return render(request, self.html, {'form': form})
